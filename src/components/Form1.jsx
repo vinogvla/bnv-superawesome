2e4f9b7a45a8aad83f0c4ce2f4f7fa8c05acc6df
Vladimir Vinogradov <vinogvla@gmail.com>
2018-03-13 17:21:21 -0400

    Add bootstrap

--- src/components/Form1.jsx
+++ src/components/Form1.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
-  
+import { Button, FormGroup, FormControl, ControlLabel, HelpBlock } from 'react-bootstrap' 
 
 const Form = ({ email, fname, lname, isInvalid, formChange, formSubmit }) => {
 
   function handleChange (e) {
     formChange({
-      name: e.target.name,
+      name: e.target.id,
       value: e.target.value
     })
   }
 
- function handleSubmit (e) {
+  const getValidationState = () => {
+    const length = email.length
+    if (length > 10) return 'success'
+    else if (length > 5) return 'warning'
+    else if (length > 0) return 'error'
+    return null 
+  }
+
+  function handleSubmit (e) {
     e.preventDefault()
     formSubmit()
   }
@@ -20,22 +28,37 @@ const Form = ({ email, fname, lname, isInvalid, formChange, formSubmit }) => {
 
       <h2>Form 1</h2>      
 
-      <div className="formElement">
-        <label htmlFor="email">Email</label>
-        <input type="text" id="email" name="email" value={email} onChange={handleChange} />
-      </div>
+      <FormGroup
+        controlId="email"
+        validationState={getValidationState()}
+      >
+        <ControlLabel>Email</ControlLabel>
+        <FormControl type="email" value={email} placeholder="Email" onChange={handleChange} />
+        <FormControl.Feedback />
+        <HelpBlock>Please enter your email address</HelpBlock>
+      </FormGroup>
+      
+      <FormGroup
+        controlId="fname"
+        validationState={getValidationState()}
+      >
+        <ControlLabel>Fname</ControlLabel>
+        <FormControl type="text" value={fname} placeholder="Fname" onChange={handleChange} />
+        <FormControl.Feedback />
+        <HelpBlock>Please enter your fname</HelpBlock>
+      </FormGroup>
+      
+      <FormGroup
+        controlId="lname"
+        validationState={getValidationState()}
+      >
+        <ControlLabel>Lname</ControlLabel>
+        <FormControl type="text" value={lname} placeholder="lname" onChange={handleChange} />
+        <FormControl.Feedback />
+        <HelpBlock>Please enter your lname</HelpBlock>
+      </FormGroup>
 
-      <div className="formElement">
-        <label htmlFor="fname">First name</label>
-        <input id="fname" name="fname" value={fname} onChange={handleChange} />
-      </div>
-
-      <div>
-        <label htmlFor="lname">Last name</label>
-        <input id="lname" name="lname" value={lname} onChange={handleChange} />
-      </div>
-
-      <button disabled={isInvalid}>Submit</button>
+      <Button disabled={isInvalid}>Submit</Button>
 
     </form>
   )
